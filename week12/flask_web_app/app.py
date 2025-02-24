13c7b77ff4b91093a3dfea26761c0d084b490df6
Cheng Wang <270595728@yoobeestudent.ac.nz>
2025-02-25 12:01:03 +1300

    test

--- week12/flask_web_app/app.py
+++ week12/flask_web_app/app.py
@@ -13,5 +13,20 @@ def mySecondPage():
     return render_template('mySecondPage.html')
 
 
+@app.route('/username/<name>')
+def learn(name):
+    return f"{name} is learning Flask"
+
+
+@app.route('/<name>/<int:number>')
+def learn2(name, number):
+    return f"{name} is learning Flask! He wakes up at {number} every day!"
+
+
+@app.route('/mySecondPage/<age>')
+def mySecondPage1(age):
+    return render_template('mySecondPage.html', age=age)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
